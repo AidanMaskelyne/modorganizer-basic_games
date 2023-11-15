306dffdbf15cdb7c24f94d2b435490c08466fe67
Aidan <aidan@muttleyville.org>
2023-11-15 16:33:08 +0000

    Add basic Ready or Not support and fix(?) Crusader Kings III plugin

---
+++ games/game_ready-or-not.py
@@ -0,0 +1,14 @@
+from ..basic_game import BasicGame
+
+class ReadyOrNotGame(BasicGame):
+	Name = "Ready or Not Support Plugin"
+	Author = "Aidan Maskelyne"
+	Version = "1.0.0"
+
+	GameName = "Ready or Not"
+	GameShortName = "readyornot"
+	GameNexusName = "readyornot"
+	GameNexusId = 4205
+	GameSteamId = 1144200
+	GameBinary = "ReadyOrNot.exe"
+	GameDataPath = "ReadyOrNot/Content/Paks"
