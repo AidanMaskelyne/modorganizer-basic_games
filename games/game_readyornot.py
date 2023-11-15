ae072d1f362f1fddf64c6400d42bc8470fb56321
Aidan <aidan@muttleyville.org>
2023-11-15 16:37:18 +0000

    Fix git shenanigans and rename Ready or Not plugin filename

--- games/game_readyornot.py
+++ games/game_readyornot.py
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-=======
-from PyQt6.QtCore import QDir
-
->>>>>>> 8f24aafe65f081a400900b108a74a6081e1a5350
 from ..basic_game import BasicGame
 
 class ReadyOrNotGame(BasicGame):
 	Name = "Ready or Not Support Plugin"
 	Author = "Aidan Maskelyne"
-<<<<<<< HEAD
 	Version = "1.0.0"
-=======
-	Version = "0.0.1"
->>>>>>> 8f24aafe65f081a400900b108a74a6081e1a5350
 
 	GameName = "Ready or Not"
 	GameShortName = "readyornot"
@@ -20,8 +11,4 @@ class ReadyOrNotGame(BasicGame):
 	GameNexusId = 4205
 	GameSteamId = 1144200
 	GameBinary = "ReadyOrNot.exe"
-<<<<<<< HEAD
 	GameDataPath = "ReadyOrNot/Content/Paks"
-=======
-	GameDataPath = "ReadyOrNot/Content/Paks"
->>>>>>> 8f24aafe65f081a400900b108a74a6081e1a5350
